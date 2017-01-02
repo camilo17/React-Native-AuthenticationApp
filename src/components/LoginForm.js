ada409ab130958dfff5bfbcccc1c5b99f80bbf5f
Camilo Guerrero <cami172013@gmail.com>
2017-01-02 10:04:49 -0600

    Added firebase, startedon LoginForm Component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {
+    View
+} from 'react-native';
+
+export default class LoginForm extends Component {
+    render()  {
+        return(
+            <View></View>
+        )
+    }
+}
