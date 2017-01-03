5791370fa21c1afc817b10ddd818c150224fbf69
Camilo Guerrero <cami172013@gmail.com>
2017-01-03 14:29:30 -0600

    Added TextInputinto Loginform with some style.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
 import {
-    View
+    View,
+    TextInput
 } from 'react-native';
+
+import {Button, Card, CardSection} from './common';
 
 export default class LoginForm extends Component {
     render()  {
         return(
-            <View></View>
+            <Card>
+                <CardSection>
+                    <TextInput style="{{height: 20, width: 100}}"></TextInput>
+
+                </CardSection>
+
+                <CardSection>
+
+                </CardSection>
+
+
+                <CardSection>
+                    <Button>
+                        Log In
+                    </Button>
+                </CardSection>
+
+            </Card>
         )
     }
 }
